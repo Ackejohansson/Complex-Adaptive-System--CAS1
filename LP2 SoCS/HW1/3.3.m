6479792f2a9312c2da24911c3b9657f267f8bfb6
ackejohansson <axelgbg@live.se>
2022-11-08 19:02:00 +0100

    test

--- LP2 SoCS/HW1/3.3.m
+++ LP2 SoCS/HW1/3.3.m
@@ -1,40 +1,23 @@
 %% 3.3
-clc, clf
-N=100;
-alpha = 1.18;
+clc, clf, clear
+N=16;
+alpha = 1.5;
 n = linspace(1,N^2,N^2);
-
 p=1./n.^alpha;
 a=sum(p);
-
-
 for k = 1:N^2
    c(k) = sum(p(k:end))/a;
 end
 loglog(n/N^2,c,'o')
 
-%% 3.3 TEST HIGH PERFORMANCE
+
+
+%% 3.3 Test performance
 clc, clf
-N=10;
+N=106;
 alpha = 1.18;
 n = linspace(1,N^2,N^2);
-p=1./n.^alpha;
-a=sum(p);
 
-for k = 1:N^2
-   c(k) = sum(p(k:end))/a;
-end
+c = n.^1/((1-alpha))
 loglog(n/N^2,c,'o')
 
-%% 3.3 TEST HIGH PERFORMANCE
-clc, clf
-N=10;
-alpha = 1.18;
-n = linspace(1,N^2,N^2);
-p=1./n.^alpha;
-a=sum(p);
-
-for k = 1:N^2
-   c(k) = sum(p(k:end))/a;
-end
-loglog(n/N^2,c,'o')
