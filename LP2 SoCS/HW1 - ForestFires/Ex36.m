aadf4ac5994a3a7ab94d42d79af481955714946d
ackejohansson <axelgbg@live.se>
2022-11-14 22:38:44 +0100

    tom 36

--- LP2 SoCS/HW1 - ForestFires/Ex36.m
+++ LP2 SoCS/HW1 - ForestFires/Ex36.m
@@ -3,31 +3,50 @@ clf, clc, clear
 N=256;
 p=0.01;
 f=0.2;
-T = 1e4;
+T = 1e5;
 
 [fireSizes, forestSizes] = SimulateForestFire(N,p,f,T);
 histogram(fireSizes)
 n = (1:size(fireSizes,2))/size(fireSizes,2);
 sortedFireSizes = sort(fireSizes, 'descend')./N^2;
-
+    
 %%
-clf, clc
+clf
 loglog(sortedFireSizes,n,'b')
 hold on
-index = sortedFireSizes <0.2;
+index = sortedFireSizes < 0.20;
 x = sortedFireSizes(index);
 y = n(index);
+
+b=1;
+x = x(b:end);
+y=y(b:end);
 c = polyfit(log10(x),log10(y),1);
 beta = c(1);
 
 loglog(sortedFireSizes, 10^(c(2))*sortedFireSizes.^beta,'r')
+xlabel("n/N^2")
+ylabel("C(n)")
 alpha=1-beta
+txtCurve = 'Simulation of forest fires';
+txtFit = sprintf('Curve fitted: \\alpha = %.4f',alpha);
+legend(txtCurve,txtFit, 'Location','west')
+hold off
+%%
+%Settings for alpha
+clf
+loglog(sortedFireSizes,n,'b')
+hold on
+alpha = 1.169;
+x=linspace(1/N^2, 1,N^2);
+a = (x.^(1-alpha));
+a=a./a(1);
+plot(x,a,'r')
+xlabel("n/N^2")
+ylabel("C(n)")
 
-
-% Settings for alpha
-%alpha = 1.169;
-% x=linspace(1/N^2, 1,N^2);
-% a = (x.^(1-alpha));
-% a=a./a(1);
-% plot(x,a,'r')
-
+txtCurve = 'Simulation of forest fires';
+txtFit = sprintf('Curve fitted: \\alpha = %.4f',alpha);
+legend(txtCurve,txtFit, 'Location','west')
+hold off
+    
