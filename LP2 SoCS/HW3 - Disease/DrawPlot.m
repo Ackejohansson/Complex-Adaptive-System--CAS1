df99b7359862e7e3887c07657666cd09ef4bc00e
ackejohansson <axelgbg@live.se>
2022-11-24 14:55:19 +0100

    added plots for evolution

--- LP2 SoCS/HW3 - Disease/DrawPlot.m
+++ LP2 SoCS/HW3 - Disease/DrawPlot.m
@@ -1,12 +1,9 @@
-function DrawPlot(agents,L)
-I = agents(:,3) == -1;
-S = agents(:,3) == 0;
-R = agents(:,3) == 1;
+function DrawPlot(agents,L,I,S,R)
 clf
-plot(agents(I,1),agents(I,2),'r.')
+plot(agents(I(:,end),1),agents(I(:,end),2),'r.')
 hold on
-plot(agents(S,1),agents(S,2),'b.')
-plot(agents(R,1),agents(R,2),'g.')
+plot(agents(S(:,end),1),agents(S(:,end),2),'b.')
+plot(agents(R(:,end),1),agents(R(:,end),2),'g.')
 axis([0 L 0 L])
 drawnow;
 end
--- LP2 SoCS/HW3 - Disease/ForesetMain.m
+++ LP2 SoCS/HW3 - Disease/ForesetMain.m
@@ -1,8 +1,9 @@
 %% 11.1
 clc, clf, clear
 % Population settings
-N = 300;
+N = 200;
 L = 20;
+t=1;
 
 % Disease settings
 gamma = 0.10;
@@ -10,6 +11,9 @@ d = 0.8;
 beta = 0.5;
 
 % Initialize population
+I = logical([]);
+S=logical([]);
+R=logical([]);
 agents = zeros(N,3);
 agents(:,1:2) = randi(L,N,2);
 agents(1:10,3) = -1;
@@ -18,7 +22,22 @@ while sum(agents(:,3)==-1)>0
     agents = Move(agents,d,L);
     agents = Infect(agents,beta);
     agents = Recover(agents,gamma);
-    DrawPlot(agents,L)
     
+    I(:,end+1) = agents(:,3) == -1;
+    S(:,end+1) = agents(:,3) == 0;
+    R(:,end+1) = agents(:,3) == 1;
+    
+    DrawPlot(agents,L,I,S,R)
     % add sum of S, I and R
 end
+%
+Iev = sum(I,1);
+Sev = sum(S,1);
+Rev = sum(R,1);
+%
+figure(2)
+plot(1:length(Iev),Sev, 'b')
+hold on
+plot(1:length(Iev),Iev,'r')
+plot(1:length(Iev),Rev,'g')
+legend("S","I","R")
