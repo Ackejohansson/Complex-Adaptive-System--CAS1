67f387126612afdf94ee8bdda3df540dea396b34
ackejohansson <axelgbg@live.se>
2022-11-24 16:12:26 +0100

    Manipulate HS2

--- LP2 SoCS/HW3 - Disease/DeseaseMain.m
+++ LP2 SoCS/HW3 - Disease/DeseaseMain.m
@@ -1,14 +1,14 @@
 %% 11.1
 clc, clf, clear
 % Population settings
-N = 200;
-L = 20;
+N = 1000;
+L = 100;
 t=1;
 
 % Disease settings
-gamma = 0.10;
+gamma = 0.01;
 d = 0.8;
-beta = 0.5;
+beta = 0.6;
 
 % Initialize population
 I = logical([]);
@@ -28,16 +28,21 @@ while sum(agents(:,3)==-1)>0
     R(:,end+1) = agents(:,3) == 1;
     
     DrawPlot(agents,L,I,S,R)
-    % add sum of S, I and R
 end
-%
+
+% Compute evolution
 Iev = sum(I,1);
 Sev = sum(S,1);
 Rev = sum(R,1);
-%
+
+% Plot evolution
 figure(2)
-plot(1:length(Iev),Sev, 'b')
+plot(1:length(Iev),Sev,'b')
 hold on
 plot(1:length(Iev),Iev,'r')
 plot(1:length(Iev),Rev,'g')
+axis([0 length(Iev) 0 N])
+xlabel("t")
+ylabel("Number of agents")
 legend("S","I","R")
+title("Time evolution of disease")
