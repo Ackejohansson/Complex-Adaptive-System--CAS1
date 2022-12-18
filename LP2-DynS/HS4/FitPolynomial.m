27ff7ebf484356b4ec7a5ecb2c0a3fa0791dfc7c
ackejohansson <axelgbg@live.se>
2022-12-18 01:29:48 +0100

    test

--- LP2-DynS/HS4/FitPolynomial.m
+++ LP2-DynS/HS4/FitPolynomial.m
@@ -3,7 +3,6 @@ D = zeros(length(epsilon),2);
 D(1,:) = polyfit(log(1./epsilon),log(Iq(1,:)),1);
 D(2,:) = polyfit(log(1./epsilon),Iq(2,:),1);
 D(3,:) = polyfit(log(1./epsilon),-log(Iq(3,:)),1);
-
 for i = 1:length(epsilon)-3
     D(i+3,:) = polyfit(log(1./epsilon), log(Iq(i+3,:))/(1-Q(i+3)),1);
 end
