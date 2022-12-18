575cb4a3a6cc9b70c16ccca0cae8b7af21acc92f
ackejohansson <axelgbg@live.se>
2022-12-18 18:44:19 +0100

    vectorized, removed outer forloop

--- LP2-DynS/HS4/CreateHenonMap.m
+++ LP2-DynS/HS4/CreateHenonMap.m
@@ -4,12 +4,13 @@ xList = zeros(nrOfInitConditions, nrOfIterations);
 yList = zeros(nrOfInitConditions, nrOfIterations);
 xList(:,1) = linspace(-0.1, 0.1, nrOfInitConditions);
 yList(:,1) = linspace(-0.1, 0.1, nrOfInitConditions);
-for i = 1:nrOfInitConditions
-    for j=1:nrOfIterations-1
-        xList(i, j+1) = yList(i,j) + 1 - a * xList(i,j)^2;
-        yList(i, j+1) = b*xList(i,j);
-    end
+
+for j=1:nrOfIterations-1
+    xList(:, j+1) = yList(:,j) + 1 - a * xList(:,j).^2;
+    yList(:, j+1) = b*xList(:,j);
 end
+
 xList(:,1:transient) = [];
 yList(:,1:transient) = [];
 end
+
