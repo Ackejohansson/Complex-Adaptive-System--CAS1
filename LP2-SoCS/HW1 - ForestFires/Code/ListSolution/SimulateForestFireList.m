02bb052d51ff1ee5eb7bb90193c13ee5d5831e2e
ackejohansson <axelgbg@live.se>
2022-12-20 19:15:32 +0100

    small change

--- LP2-SoCS/HW1 - ForestFires/Code/ListSolution/SimulateForestFireList.m
+++ LP2-SoCS/HW1 - ForestFires/Code/ListSolution/SimulateForestFireList.m
@@ -7,9 +7,9 @@ for t=0:T
        for j=1:N
           if rand<p
               forest(i,j)=1;
-%               hold on
-%               fill([0 1 1 0]+i-1, [0 0 1 1]+j-1, 'g')
-              %hold off
+               hold on
+               fill([0 1 1 0]+i-1, [0 0 1 1]+j-1, 'g')
+              hold off
           end
        end
    end
@@ -17,21 +17,21 @@ for t=0:T
 [fireSizes, forestSizes, forest, fire] = LightAndBurnList(f,forest,fireSizes,forestSizes,N);
 forest(forest == -1)=0;
 
-% For drawing 
-% if fire
-%     [a,b] = find(forest == -1);
-%     hold on
-%     for k = 1:length(a)
-%         fill([0 1 1 0] + a(k)-1, [0 0 1 1] + b(k)-1, 'r')
-%     end
-%     drawnow;
-%     pause(0.5) 
-%     for k = 1:length(a)
-%         forest(a(k),b(k))=0;
-%         fill([0 1 1 0]+a(k)-1, [0 0 1 1]+b(k)-1, 'w')
-%     end
-%     drawnow;
-%     pause(0.3) 
-% end
+%For drawing 
+if fire
+    [a,b] = find(forest == -1);
+    hold on
+    for k = 1:length(a)
+        fill([0 1 1 0] + a(k)-1, [0 0 1 1] + b(k)-1, 'r')
+    end
+    drawnow;
+    pause(0.5) 
+    for k = 1:length(a)
+        forest(a(k),b(k))=0;
+        fill([0 1 1 0]+a(k)-1, [0 0 1 1]+b(k)-1, 'w')
+    end
+    drawnow;
+    pause(0.3) 
+end
 end
 end
