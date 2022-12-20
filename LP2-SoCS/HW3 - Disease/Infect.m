da99824deeffe726a3bbad5c50c21195efd3ba26
ackejohansson <axelgbg@live.se>
2022-12-20 23:19:05 +0100

    trying to remove find(ismember(...)), WIP

--- LP2-SoCS/HW3 - Disease/Infect.m
+++ LP2-SoCS/HW3 - Disease/Infect.m
@@ -1,5 +1,5 @@
 function agents = Infect(agents,beta)
-infected = agents( agents(:,3) == -1, 1:2);
+infected = agents(agents(:,3) == -1, 1:2);
 
 for i = 1:size(infected,1)
     [samePosition,~] = find(ismember(agents(:,1:2),infected(i,:),'rows'));
@@ -7,5 +7,17 @@ for i = 1:size(infected,1)
     index=(rand(size(samePosition,1),1)<beta) & suceptable;
     agents(samePosition(index),3) = -1;
 end
+
+%%% Work in progress, trying to remove find(ismember(...))
+%
+% infectedVector = agents(:,3) == -1;
+% infectedPos = agents(infectedVector, 1:2);
+% 
+% for i = 1:size(infectedPos,1)
+%    samePos = sum(infectedPos(i,:)==infectedPos, 2) == 2
+% end
+% 
+% sick = agents(infected,1:2);
+% samePos = sum(sick(i,:) == agents(:,1:2), 2) ==2;
 end
 
