26abe9d59bf1bd7608f0ec5b145a7b19c289ebcf
ackejohansson <axelgbg@live.se>
2022-12-20 22:34:26 +0100

    Vectorized, 10x perfomance

--- LP2-SoCS/HW3 - Disease/Move.m
+++ LP2-SoCS/HW3 - Disease/Move.m
@@ -1,6 +1,7 @@
 function agents = Move(agents,d,L)
 move = (rand(length(agents),1) < d).*randi(4,length(agents),1);
 
+% Move 1: right, 2: up, 3: left, 4: down
 agents(move == 1,1) = agents(move == 1,1) +1;
 agents(move == 2,2) = agents(move == 2,2) +1;
 agents(move == 3,1) = agents(move == 3,1) -1;
