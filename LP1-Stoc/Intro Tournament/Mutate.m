201ef7c6277c0757100c55f512d99153123ee39e
ackejohansson <axelgbg@live.se>
2022-12-22 22:43:24 +0100

    vectorized

--- LP1-Stoc/Intro Tournament/Mutate.m
+++ LP1-Stoc/Intro Tournament/Mutate.m
@@ -1,12 +1,7 @@
-function mutatedChromosome = Mutate(chromosome, mutationProbability)
-    
-    nGenes = size(chromosome,2);
-    mutatedChromosome = chromosome;
-    for i = 1:nGenes
-        r = rand;
-        if r < mutationProbability
-            mutatedChromosome(i) = 1 - chromosome(i);
-        end
-    end
-    
+function mutatedChromosome = Mutate(chromosome, mutationProbability) 
+nGenes = size(chromosome,2);
+mutatedChromosome = chromosome;
+
+mutate = rand(1,nGenes) < mutationProbability;
+mutatedChromosome(mutate) = double(~mutatedChromosome(mutate));
 end
