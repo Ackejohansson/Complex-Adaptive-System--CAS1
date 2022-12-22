ae0d835317fddec59675662e55fa149268797208
ackejohansson <axelgbg@live.se>
2022-12-22 21:43:05 +0100

    small changes

--- LP1-Stoc/Intro Tournament/InitializePopulation.m
+++ LP1-Stoc/Intro Tournament/InitializePopulation.m
@@ -1,14 +1,13 @@
 function population = InitializePopulation(populationSize, nGenes)
 
- population = zeros(populationSize, nGenes);
+population = zeros(populationSize, nGenes);
 for i = 1:populationSize
     for j = 1:nGenes
-        s = rand;
-            if (s < 0.5)
+        if (rand < 0.5)
             population(i,j)=0;
-            else
+        else
             population(i,j)=1;
-            end
+        end
     end
 end
 end
