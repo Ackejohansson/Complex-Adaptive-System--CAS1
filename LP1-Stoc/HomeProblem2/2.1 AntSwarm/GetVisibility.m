4445579d3597386429df221df880dac2815ce134
ackejohansson <axelgbg@live.se>
2022-12-23 15:25:45 +0100

    vectorized

--- LP1-Stoc/HomeProblem2/2.1 AntSwarm/GetVisibility.m
+++ LP1-Stoc/HomeProblem2/2.1 AntSwarm/GetVisibility.m
@@ -1,6 +1,3 @@
 function visibility = GetVisibility(cityLocation)
-visibility = zeros(length(cityLocation));
-for i = 1:length(cityLocation)
-    visibility(i,:) = 1./pdist2(cityLocation, cityLocation(i,:));
-end
+visibility = 1./pdist2(cityLocation, cityLocation);
 end
