31669fab8399e334f1f16b6aae3ef45c26c19b85
ackejohansson <axelgbg@live.se>
2022-12-23 15:46:09 +0100

    40x performance vectorized

--- LP1-Stoc/HomeProblem2/2.1 AntSwarm/GetVisibility.m
+++ LP1-Stoc/HomeProblem2/2.1 AntSwarm/GetVisibility.m
@@ -1,3 +1,3 @@
 function visibility = GetVisibility(cityLocation)
-visibility = 1./pdist2(cityLocation, cityLocation);
+visibility = 1 ./sqrt((cityLocation(:,1) - cityLocation(:,1)').^2 + (cityLocation(:,2) - cityLocation(:,2)').^2);
 end
