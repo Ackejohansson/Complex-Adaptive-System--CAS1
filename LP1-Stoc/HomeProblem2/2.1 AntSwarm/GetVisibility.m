b779740a2e37c8f716e6f24b3ce1f191f2243803
ackejohansson <axelgbg@live.se>
2022-12-23 15:19:03 +0100

    radability and vectorized

--- LP1-Stoc/HomeProblem2/2.1 AntSwarm/GetVisibility.m
+++ LP1-Stoc/HomeProblem2/2.1 AntSwarm/GetVisibility.m
@@ -1,8 +1,6 @@
 function visibility = GetVisibility(cityLocation)
-    visibility = zeros(size(cityLocation, 1));
-    for i = 1:length(cityLocation)
-        for j = 1:length(cityLocation)
-            visibility(i,j) = 1 / sqrt((cityLocation(i,2)-cityLocation(j,2))^2 +(cityLocation(i,1)-cityLocation(j,1))^2);
-        end
-    end
+visibility = zeros(length(cityLocation));
+for i = 1:length(cityLocation)
+    visibility(i,:) = 1./pdist2(cityLocation, cityLocation(i,:));
+end
 end
--- LP1-Stoc/HomeProblem2/2.1 AntSwarm/InitializeTspPlot.m
+++ LP1-Stoc/HomeProblem2/2.1 AntSwarm/InitializeTspPlot.m
@@ -1,5 +1,4 @@
 function tspFigure = InitializeTspPlot(cityLocation,range)
-
 tspFigure = figure(1);
 set(tspFigure,'DoubleBuffer','on');
 axis([range(1),range(2),range(3),range(4)]);
@@ -8,5 +7,4 @@ grid on;
 city = [];
 for i = 1:length(cityLocation)
    city(i) = rectangle('Position', [cityLocation(i,1)-0.25 cityLocation(i,2)-0.25 0.5 0.5],'Curvature', [1 1]);
-end
-   
+end 
