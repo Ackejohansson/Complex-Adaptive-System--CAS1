d8b4a828cfd4708ab78253fb5313455a388b3204
ackejohansson <axelgbg@live.se>
2022-12-29 10:48:07 +0100

    test

--- LP1-Stoc/HomeProblem2/2.1 AntSwarm/GetNode.m
+++ LP1-Stoc/HomeProblem2/2.1 AntSwarm/GetNode.m
@@ -1,23 +1,23 @@
 function nextNode = GetNode(path, i, pheromoneLevel, visibility, alpha, beta)
-    nNodes = size(visibility,1);
-    P = zeros(nNodes,1);
-    
-    % Create probability vector
-    for j = 1:nNodes
-        if ~ismember(j,path)
-            P(j) = (pheromoneLevel(j,path(i)).^alpha .* visibility(j,path(i)).^beta);
-        end
+nNodes = size(visibility,1);
+P = zeros(nNodes,1);
+
+% Create probability vector
+for j = 1:nNodes
+    if ~ismember(j,path)
+        P(j) = (pheromoneLevel(j,path(i)).^alpha .* visibility(j,path(i)).^beta);
     end
-    P = P / sum(P);
-    
-    % Choose nextNode w. RWS 
-    Psum = 0;
-    r = rand;
-    for k = 1:nNodes
-        Psum = Psum + P(k);
-        if r <= Psum
-            nextNode = k;
-            return
-        end
-    end     
+end
+P = P / sum(P);
+
+% Choose nextNode w. RWS 
+Psum = 0;
+r = rand;
+for k = 1:nNodes
+    Psum = Psum + P(k);
+    if r <= Psum
+        nextNode = k;
+        return
+    end
+end     
 end
