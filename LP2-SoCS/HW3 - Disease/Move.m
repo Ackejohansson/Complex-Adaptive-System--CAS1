4a9db9e9cab4ff2219a6ffb1b5b7753508a8143e
ackejohansson <axelgbg@live.se>
2022-12-30 17:24:58 +0100

    right left up down instead of number for readability

--- LP2-SoCS/HW3 - Disease/Move.m
+++ LP2-SoCS/HW3 - Disease/Move.m
@@ -1,14 +1,16 @@
-function agents = Move(agents,d,L)
-move = (rand(length(agents),1) < d).*randi(4,length(agents),1);
+function agents = Move(agents,movementProb,L)
+move = randsample(["right","up","left","down"],length(agents),true);
+move(rand(length(agents),1) > movementProb) = 0;
 
-% Move 1: right, 2: up, 3: left, 4: down
-agents(move == 1,1) = agents(move == 1,1) +1;
-agents(move == 2,2) = agents(move == 2,2) +1;
-agents(move == 3,1) = agents(move == 3,1) -1;
-agents(move == 4,2) = agents(move == 4,2) -1;
+agents(move == "right",1) = agents(move == "right",1) +1;
+agents(move == "up",2)    = agents(move == "up",2)    +1;
+agents(move == "left",1)  = agents(move == "left",1)  -1;
+agents(move == "down",2)  = agents(move == "down",2)  -1;
 
 tmpAgents = agents(:,1:2);
 tmpAgents(tmpAgents>L) = tmpAgents(tmpAgents>L)-L;
 tmpAgents(tmpAgents<0) = tmpAgents(tmpAgents<0)+L;
 agents(:,1:2) = tmpAgents;
 end
+
+
