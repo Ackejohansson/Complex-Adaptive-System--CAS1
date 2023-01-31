9de8342cb93654ad087f6cbb75b2a7b2000b3474
ackejohansson <axelgbg@live.se>
2023-01-31 11:43:11 +0100

    task 3 almost done

--- LP3-CompBio/ProblemSets/PS1.py
+++ LP3-CompBio/ProblemSets/PS1.py
@@ -8,14 +8,13 @@ def main():
     A = 20
     K = 100
     r = 0.1
-    ts = np.linspace(0, 20, 1000)
+    ts = np.linspace(0, 3.0, num=5)
     ns = ddeint(Nprim, initial_population, ts, fargs=(r, A, K, T))
     plt.plot(ts, ns)
     plt.show()
 
-
-def Nprim(N, t, r, A, K, T):
-    return r*N(t)*(1-N(t-T)/K)*(N(t)/A-1)
+def Nprim(population, t, r, a, k, time_delay):
+    return r*population(t)*(1 - population(t - time_delay) / k)*(population(t) / a - 1)
 
 
 def initial_population(t):
