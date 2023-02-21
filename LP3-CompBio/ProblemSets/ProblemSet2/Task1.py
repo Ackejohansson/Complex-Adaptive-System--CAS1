ca32bb2de168e3d21f06fb58401c826ee8e170b1
ackejohansson <axelgbg@live.se>
2023-02-21 16:44:52 +0100

    Finnalized

--- LP3-CompBio/ProblemSets/ProblemSet2/Task1.py
+++ LP3-CompBio/ProblemSets/ProblemSet2/Task1.py
@@ -10,27 +10,46 @@ def ramp(u0, xi, xi_0, L, time_steps, dt):
     return u
 
 
-def simulation(q, u, dt, rho, time_steps, h):
+def ramp_c(uc0, xi, xic_0, time_steps, dt, L):
+    uc = np.zeros((2, L, int(time_steps / dt)))
+    uc[0, :, 0] = uc0[0] * np.exp(-(xi - xic_0[0]) ** 2)
+    uc[1, :, 0] = uc0[1] * np.exp(-(xi - xic_0[1]) ** 2)
+    return uc
+
+
+def simulation(q, u, dt, rho, time_steps):
     for t in range(int(time_steps / dt - 1)):
-        u[:,:h,t+1] = u[:,:h,t] + dt * (
-                rho*u[:,:h,t] * (1-u[:,:h,t]/q) - u[:,:h,t]/(1+u[:,:h,t]))
+        u[:, 0, t + 1] = u[:, 0, t] + dt * (
+                rho * u[:, 0, t] * (1 - u[:, 0, t] / q) - u[:, 0, t] / (1 + u[:, 0, t]) + (u[:, 1, t] - u[:, 0, t]).T)
 
-        u[:,-h:,t+1] = u[:,-h:,t] + dt * (
-                rho*u[:,-h:,t] * (1-u[:,-h:,t]/q) - u[:,-h:,t]/(1+u[:,-h:,t]))
+        u[:, -1, t + 1] = u[:, -1, t] + dt * (
+                rho * u[:, -1, t] * (1 - u[:, -1, t] / q) - u[:, -1, t] / (1 + u[:, -1, t]) - (
+                u[:, -1, t] - u[:, -2, t]))
 
-        u[:,h:-h,t+1] = u[:,h:-h,t] + dt * (
-                rho*u[:,h:-h,t] * (1-u[:,h:-h,t]/q) - u[:,h:-h,t] / (1+u[:,h:-h,t]) +
-                (u[:,:-h-1,t] + u[:,h+1:,t] - 2*u[:,h:-h,t])/h**2)
+        u[:, 1:-1, t + 1] = u[:, 1:-1, t] + dt * (
+                rho * u[:, 1:-1, t] * (1 - u[:, 1:-1, t] / q) - u[:, 1:-1, t] / (1 + u[:, 1:-1, t]) +
+                (u[:, :-2, t] + u[:, 2:, t] - 2 * u[:, 1:-1, t]))
+
     return u
 
 
 def draw_b(u, time_steps, dt):
-    plt.figure()
-    plt.plot(u[0, :, :])
+    #plt.figure()
+    #plt.plot(u[0, :, :])
     plt.show()
-    for i in np.linspace(0, time_steps/dt - 1, 30):
-        plt.plot(u[0, :, int(i)])
+    print(len(u))
+    for j in range(len(u)):
+        plt.figure()
+        for i in np.linspace(0, 0.2*time_steps / dt - 1, 30):
+            plt.plot(u[j, :, int(i)])
     plt.show()
+
+
+def wave_speed(u, dt):
+    print(np.argmax(u[0, 60, :] > 2))
+    print(np.argmax(u[0, 50, :] > 2))
+    print(10 / ((np.argmax(u[0, 60, :] > 2) - np.argmax(u[0, 50, :] > 2)) * dt))
+
 
 def main():
     rho = .5
@@ -39,17 +58,22 @@ def main():
     q = 8
     xi = np.arange(1, L + 1)
     xi_0 = np.array([20, 50, 50])
-    u0 = np.array([(q-1)/2 + np.sqrt(((q-1)/2)**2 - q*(1-rho)/rho),
-                   (q-1)/2 - np.sqrt(((q-1)/2)**2 - q*(1-rho)/rho),
-                   1.1 * ((q-1)/2 - np.sqrt(((q-1)/2)**2 - q*(1-rho)/rho))])
+    u0 = np.array([(q - 1) / 2 + np.sqrt(((q - 1) / 2) ** 2 - q * (1 - rho) / rho),
+                   (q - 1) / 2 - np.sqrt(((q - 1) / 2) ** 2 - q * (1 - rho) / rho),
+                   1.1 * ((q - 1) / 2 - np.sqrt(((q - 1) / 2) ** 2 - q * (1 - rho) / rho))])
     time_steps = 500
-    dt = 1/10
-    h = 1
-
+    dt = 1/100
     u = ramp(u0, xi, xi_0, L, time_steps, dt)
-    u = simulation(q, u, dt, rho, time_steps, h)
-    draw_b(u, time_steps, dt)
+    u = simulation(q, u, dt, rho, time_steps)
+    #draw_b(u, time_steps, dt)
 
+    uc0 = np.array([u0[0], u0[0]*3])
+    xic_0 = np.array([50, 50])
+    #uc = ramp_c(uc0, xi, xic_0, time_steps, dt, L)
+    #uc = simulation(q, uc, dt, rho, time_steps)
+    #draw_b(uc, time_steps, dt)
+
+    wave_speed(u, dt)
 
 if __name__ == '__main__':
     main()
