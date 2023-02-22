deecaa028e75e92cc60c16ca735b052a298ec02d
ackejohansson <axelgbg@live.se>
2023-02-22 13:17:35 +0100

    laplacian

--- LP3-CompBio/ProblemSets/ProblemSet2/Task2.py
+++ LP3-CompBio/ProblemSets/ProblemSet2/Task2.py
@@ -2,27 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def update_u(u, b, v, a):
-    u += a -(b+1)*u + u**2 @ v
-    
-def update_v(b, u, v):
-    v += b*u- u**2 @ v
+def laplace(grid):
+    return np.roll(grid,1,axis=0) + np.roll(grid,-1,axis=0) + np.roll(grid,1,axis=1) + np.roll(grid,-1,axis=1) - 4*grid
+
+
+def update_grid(u, b, v, a):
+    u += a - (b + 1) * u + u ** 2 @ v + laplace(u)
+    v += b * u - u ** 2 @ v + laplace(v)
+
 
 def main():
     L = 128
+    Du = 1
     Dv = np.array([2.3, 3, 5, 9])
     a = 3
     b = 8
     steady_u = a
     steady_v = b / a
-    grid = np.zeros((L,L))
+    grid = np.zeros((L, L))
     u = np.random.uniform(low=0.9 * steady_u, high=1.1 * steady_u, size=(L, L))
     v = np.random.uniform(low=0.9 * steady_v, high=1.1 * steady_v, size=(L, L))
-
-    for t in
-        du = a-(b+1)*u + u**2 * v
-        dv = b*u -u**2 *v
-
 
 
 if __name__ == '__main__':
