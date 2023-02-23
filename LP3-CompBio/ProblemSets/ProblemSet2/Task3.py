96703d3046f534d37b7612fb8244af20fff1ff9f
ackejohansson <axelgbg@live.se>
2023-02-23 16:27:07 +0100

    middle k not converging

--- LP3-CompBio/ProblemSets/ProblemSet2/Task3.py
+++ LP3-CompBio/ProblemSets/ProblemSet2/Task3.py
@@ -6,35 +6,35 @@ from scipy.stats import cauchy
 
 def update_theta(theta, k, N, omega, dt):
     diff = theta[:, np.newaxis] - theta[np.newaxis, :]
-    theta += dt * (omega + k / N * (np.sin(diff)).sum(axis=0))
+    theta += dt * (omega + k/N * (np.sin(diff)).sum(axis=0))
     return theta
 
 
 def calculate_r(theta, N):
-    return np.abs(np.sum(np.exp(1j * theta)) / N)
+    return np.absolute(np.sum(np.exp(1j * theta)))/N
 
 
 def main():
     gamma = .1
     N = np.array([20, 100, 300])
-    N = 20
-    Kc = 2 * gamma
-    K = np.array([0.08,1.01, 2])*Kc
-    dt = 1/100
-    time_max = 50
-    r = np.zeros((3,int(time_max/dt)))
+    N = 100
+    K = np.array([0.1, 0.3, 1])
+    dt = 1/10
+    time_max = 500
+    r = np.zeros((3, int(time_max / dt)))
 
-    omega = cauchy.rvs(scale=0.1, size=N) * gamma / np.pi #TODO change this guy
-    theta = np.random.uniform(low=-np.pi/2, high=np.pi/2, size=N)
-
-    fig, axes = plt.subplots(1,3, constrained_layout=True, figsize=(10,3))
-    for i,k in enumerate(K):
+    fig, axes = plt.subplots(1, 3, constrained_layout=True, figsize=(10, 3))
+    for i, k in enumerate(K):
         ax = axes[i]
-        for t in range(int(time_max/dt)):
+        omega = cauchy.rvs(loc=0, scale=gamma, size=N)
+        theta = np.random.uniform(low=-np.pi / 2, high=np.pi / 2, size=N)
+        for t in range(int(time_max / dt)):
             theta = update_theta(theta, k, N, omega, dt)
-            r[i,t] = calculate_r(theta, N)
-        ax.set_title(f"K={K[i]}")
-        ax.plot(r[i, :])
+            r[i, t] = calculate_r(theta, N)
+        ax.set_title(f"K={K[i]}, N={N}")
+        ax.plot(np.array(range(len(r[i,:])))*dt, r[i, :])
+        ax.set_xlabel("Time (t)")
+        ax.set_xlabel("Order parameter (r)")
     plt.show()
 
 
