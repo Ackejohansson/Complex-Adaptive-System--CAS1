3f762be20594793ba5e0543743af0842cb9818c6
ackejohansson <axelgbg@live.se>
2023-02-24 13:37:09 +0100

    Finnealized problem 3. Somme error in task3

--- LP3-CompBio/ProblemSets/ProblemSet2/Task3.py
+++ LP3-CompBio/ProblemSets/ProblemSet2/Task3.py
@@ -1,11 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import stats
 from scipy.stats import cauchy
 
 
 def update_theta(theta, k, N, omega, dt):
-    diff = theta[:, np.newaxis] - theta[np.newaxis, :]
+    diff = np.angle(np.exp(1j*theta[:, np.newaxis])-np.exp(1j*theta[np.newaxis, :]))
     theta += dt * (omega + k/N * (np.sin(diff)).sum(axis=0))
     return theta
 
@@ -17,20 +16,21 @@ def calculate_r(theta, N):
 def main():
     gamma = .1
     N = np.array([20, 100, 300])
-    N = 300
+    N = 20
     K = np.array([0.1, 0.23, 1])
     dt = 1/10
-    time_max = 500
+    time_max = 100
     r = np.zeros((3, int(time_max / dt)))
 
     fig, axes = plt.subplots(1, 3, constrained_layout=True, figsize=(10, 3))
     for i, k in enumerate(K):
-        ax = axes[i]
-        omega = cauchy.rvs(loc=0, scale=gamma, size=N)*0.5
+        omega = cauchy.rvs(loc=0, scale=gamma, size=N)
         theta = np.random.uniform(low=-np.pi / 2, high=np.pi / 2, size=N)
         for t in range(int(time_max / dt)):
             theta = update_theta(theta, k, N, omega, dt)
             r[i, t] = calculate_r(theta, N)
+
+        ax = axes[i]
         ax.set_title(f"K={K[i]}, N={N}")
         ax.plot(np.array(range(len(r[i,:])))*dt, r[i, :])
         ax.set_xlabel("Time (t)")
