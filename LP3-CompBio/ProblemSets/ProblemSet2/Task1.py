9b7f4f7634b1def61ff2bcc271a8e0d65bebdcde
ackejohansson <axelgbg@live.se>
2023-02-24 11:11:02 +0100

    changes plot

--- LP3-CompBio/ProblemSets/ProblemSet2/Task1.py
+++ LP3-CompBio/ProblemSets/ProblemSet2/Task1.py
@@ -37,17 +37,28 @@ def draw_b(u, time_steps, dt):
         plt.figure()
         plt.xlabel(r"$\xi$")
         plt.ylabel("Wave u")
-        for i in np.linspace(0, time_steps/dt-1, 30):
+        for i in np.linspace(0, time_steps/dt-5, 30):
             plt.plot(u[j, :, int(i)])
 
     plt.legend()
     plt.show()
 
 
+def draw_c(u, time_steps, dt):
+    for j in range(len(u)):
+        plt.figure()
+        plt.xlabel(r"$\xi$")
+        plt.ylabel("Wave u")
+        for i in np.linspace(0, time_steps/dt*0.85, 20):
+            plt.plot(u[j, :, int(i)])
+    plt.legend()
+    plt.show()
+
+
 def wave_speed(u, dt):
-    print(np.argmax(u[0, 60, :] > 2))
-    print(np.argmax(u[0, 50, :] > 2))
     print(10 / ((np.argmax(u[0, 60, :] > 2) - np.argmax(u[0, 50, :] > 2)) * dt))
+    print(10 / ((np.argmax(u[1, 30, :] < 0.5) - np.argmax(u[1, 20, :] < 0.5)) * dt))
+    print(10 / ((np.argmax(u[2, 70, :] > 2) - np.argmax(u[2, 60, :] > 2)) * dt))
 
 
 def main():
@@ -59,19 +70,33 @@ def main():
     u0 = np.array([(q - 1) / 2 + np.sqrt(((q - 1) / 2) ** 2 - q * (1 - rho) / rho),
                    (q - 1) / 2 - np.sqrt(((q - 1) / 2) ** 2 - q * (1 - rho) / rho),
                    1.1 * ((q - 1) / 2 - np.sqrt(((q - 1) / 2) ** 2 - q * (1 - rho) / rho))])
-    time_steps = 500
-    dt = 1/100
-    u = ramp(u0, xi, xi_0, L, time_steps, dt)
-    u = simulation(q, u, dt, rho, time_steps)
-    draw_b(u, time_steps, dt)
+    time_steps = 301
+    dt = 1 / 100
+    #u = ramp(u0, xi, xi_0, L, time_steps, dt)
+    #u = simulation(q, u, dt, rho, time_steps)
+    #draw_b(u, time_steps, dt)
 
     uc0 = np.array([u0[0], u0[0]*3])
     xic_0 = np.array([50, 50])
-    #uc = ramp_c(uc0, xi, xic_0, time_steps, dt, L)
-    #uc = simulation(q, uc, dt, rho, time_steps)
-    #draw_b(uc, time_steps, dt)
+    uc = ramp_c(uc0, xi, xic_0, time_steps, dt, L)
+    uc = simulation(q, uc, dt, rho, time_steps)
+    draw_c(uc, time_steps, dt)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5), constrained_layout=True)
+    #ax1.plot(u[0, :, 30000])
+    ax1.set_xlabel(r"$\xi$")
+    ax1.set_ylabel("u")
+    ax1.set_title('Travelling wave')
+
+    v = np.diff(u[0, :, 30000])
+    ax2.plot(v, u[0, :-1, 20000])
+    ax2.set_xlabel(r"$\frac{du}{d\xi}$")
+    ax2.set_ylabel("u")
+    ax2.set_title('Phase plane')
+    plt.show()
 
     wave_speed(u, dt)
+
 
 if __name__ == '__main__':
     main()
--- LP3-CompBio/ProblemSets/ProblemSet2/Task3.py
+++ LP3-CompBio/ProblemSets/ProblemSet2/Task3.py
@@ -17,8 +17,8 @@ def calculate_r(theta, N):
 def main():
     gamma = .1
     N = np.array([20, 100, 300])
-    N = 100
-    K = np.array([0.1, 0.3, 1])
+    N = 300
+    K = np.array([0.1, 0.23, 1])
     dt = 1/10
     time_max = 500
     r = np.zeros((3, int(time_max / dt)))
@@ -26,7 +26,7 @@ def main():
     fig, axes = plt.subplots(1, 3, constrained_layout=True, figsize=(10, 3))
     for i, k in enumerate(K):
         ax = axes[i]
-        omega = cauchy.rvs(loc=0, scale=gamma, size=N)
+        omega = cauchy.rvs(loc=0, scale=gamma, size=N)*0.5
         theta = np.random.uniform(low=-np.pi / 2, high=np.pi / 2, size=N)
         for t in range(int(time_max / dt)):
             theta = update_theta(theta, k, N, omega, dt)
@@ -34,7 +34,7 @@ def main():
         ax.set_title(f"K={K[i]}, N={N}")
         ax.plot(np.array(range(len(r[i,:])))*dt, r[i, :])
         ax.set_xlabel("Time (t)")
-        ax.set_xlabel("Order parameter (r)")
+        ax.set_ylabel("Order parameter (r)")
     plt.show()
 
 
