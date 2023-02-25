0daeb70b4767f8721bbc9079468b7e1de17627f2
ackejohansson <axelgbg@live.se>
2023-02-25 12:47:29 +0100

    Plot performance good. Iterating model over time takes time

--- LP3-CompBio/ProblemSets/ProblemSet2/Task2.py
+++ LP3-CompBio/ProblemSets/ProblemSet2/Task2.py
@@ -15,17 +15,17 @@ def update_v(u, b, v, Dv, dt):
     return (b * u - u ** 2 * v + Dv * laplace(v)) * dt
 
 
-def draw(u, u1000):
+def draw(u, u1000, Dv):
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5), constrained_layout=True)
     im1 = ax1.imshow(u1000, vmin=0, vmax=12, extent=[0, 128, 0, 128])
     im2 = ax2.imshow(u, vmin=0, vmax=12, extent=[0, 128, 0, 128])
-    fig.suptitle(f"Dv =")
+    fig.suptitle(f"Dv = {Dv}")
     fig.colorbar(im2, ax=ax2, shrink=0.85)
     plt.show()
 
 
 def main():
-    time_steps = 1000
+    time_steps = 100
     dt = 1/100
     L = 128
     Dv = np.array([2.3, 3, 5, 9])
@@ -44,10 +44,9 @@ def main():
         v_tmp = v + update_v(u, b, v, Dv, dt)
         u = np.copy(u_tmp)
         v = np.copy(v_tmp)
-        if t == 100:
+        if t == 200:
             u1000 = np.copy(u)
-            print("Here")
-    draw(u, u1000)  # Bad performance
+    draw(u, u1000, Dv)
 
 
 if __name__ == '__main__':
