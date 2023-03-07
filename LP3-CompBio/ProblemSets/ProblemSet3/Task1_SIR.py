e1b8861230128e09c0ee76be0b7e1b3295fbff0f
ackejohansson <axelgbg@live.se>
2023-03-07 10:21:17 +0100

    git add .

--- LP3-CompBio/ProblemSets/ProblemSet3/Task1_SIR.py
+++ LP3-CompBio/ProblemSets/ProblemSet3/Task1_SIR.py
@@ -2,14 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 bd_vals = [
-        (0.1, 0.2),
-        (1, 2),
-        (10, 5)]
+    (0.1, 0.2),
+    (1, 2),
+    (10, 5)]
 n_0 = 10  # ??
 dt = 1e-2
-iterations = int(1e5)
+iterations = int(1e3)
 b_t = np.zeros((3, iterations))
 d_t = np.zeros((3, iterations))
+
+
+def time_to_event(a):
+    t = 0
+    while True:
+        r = np.random.rand()
+        if r < a * dt: break
+        t += dt
+    return t
 
 
 def simulation(b, d, index):
@@ -18,19 +27,11 @@ def simulation(b, d, index):
         d_t[index, n] = time_to_event(d)
 
 
-def time_to_event(a):
-    t, run = 0, True
-    while run:
-        r = np.random.rand()
-        if r < a * dt:
-            break
-        t += dt
-    return t
-
 def main():
     for index in range(3):
-        b, d = bd_vals[index, :]
-        simulation(b, d)
+        b, d = bd_vals[index]
+        simulation(b, d, index)
+    print("KLAR")
 
 
 if __name__ == '__main__':
