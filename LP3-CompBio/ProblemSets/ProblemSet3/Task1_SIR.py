cca81436687f9c51183c0952868d7d025059ac22
ackejohansson <axelgbg@live.se>
2023-03-07 14:00:10 +0100

    correct plots

--- LP3-CompBio/ProblemSets/ProblemSet3/Task1_SIR.py
+++ LP3-CompBio/ProblemSets/ProblemSet3/Task1_SIR.py
@@ -29,20 +29,25 @@ def simulation(b, d, index):
         d_t[index, n] = time_to_event(d)
 
 
-def plot(index):
+def plot(index, b, d):
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5), constrained_layout=True)
 
-    n, bins, _ = ax1.hist(b_t[index, :], bins=100, log=True, density=True)
+    _, bins_b, _ = ax1.hist(b_t[index, :], bins=100, log=True, density=True, label=b)
     params_b = expon.fit(b_t[index, :], floc=0)
-    Y_b = expon.pdf(bins, *params_b)
-    ax1.plot(bins, Y_b)
-    
-    n, bins, _ = ax2.hist(d_t[index, :], bins=100, log=True, density=True)
-    params_d = expon.fit(d_t[index, :], floc=0)
-    Y_d = expon.pdf(bins, *params_d)
-    ax2.plot(bins, Y_d)
-    ax1.ylabel("Log(occurrences)")
+    Y_b = expon.pdf(bins_b, *params_b)
+    ax1.plot(bins_b, Y_b, label=1/params_b[1])
+    ax1.set_ylabel('Log( Probability )')
+    ax1.set_xlabel('Time until event')
 
+    _, bins_d, _ = ax2.hist(d_t[index, :], bins=100, log=True, density=True, label=d)
+    params_d = expon.fit(d_t[index, :], floc=0)
+    Y_d = expon.pdf(bins_d, *params_d)
+    ax2.plot(bins_d, Y_d, label=1/params_d[1])
+    ax2.set_xlabel('Time until event')
+
+    fig.suptitle('Histogram over probability distribution', fontsize=16)
+    ax1.legend()
+    ax2.legend()
     plt.show()
 
 
@@ -50,7 +55,7 @@ def main():
     for index in range(len(bd_vals)):
         b, d = bd_vals[index]
         simulation(b, d, index)
-        plot(index)
+        plot(index, b, d)
 
 
 if __name__ == '__main__':
