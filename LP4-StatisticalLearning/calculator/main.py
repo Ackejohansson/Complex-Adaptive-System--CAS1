bf98eaa655702e5eb84f905d3555f839db77863e
ackejohansson <axelgbg@live.se>
2023-03-20 09:39:00 +0100

    Added  test

---
+++ LP4-StatisticalLearning/calculator/main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Skift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def add(a, b):
+    return a - b
+
+
+if __name__ == '__main__':
+    x = add(3, 2)
+    print(x)
