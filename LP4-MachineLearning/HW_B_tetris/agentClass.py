9eb2e9120920c4b065950e831f210408b41321a6
ackejohansson <axelgbg@live.se>
2023-03-29 19:54:21 +0200

    Rewritten and still working. Working on select_action

--- LP4-MachineLearning/HW_B_tetris/agentClass.py
+++ LP4-MachineLearning/HW_B_tetris/agentClass.py
@@ -18,16 +18,23 @@ class TQAgent:
 
     def fn_init(self,gameboard):
         self.gameboard=gameboard
-        self.set_possible_actions()
+        self.set_actions() # nr_tiles times, possible moves. 1 if possible
         self.qtable = {}
         self.reward_tots = np.zeros(self.episode_count)
 
-    def set_possible_actions(self):
-        possible_actions = np.zeros((len(self.gameboard.tiles), self.gameboard.N_col*4))
-        for i, tile in enumerate(self.gameboard.tiles):
-            for j, rotation in enumerate(tile):
-                possible_actions[i][j*self.gameboard.N_col:j*self.gameboard.N_col+self.gameboard.N_col+1-len(rotation)] = 1
-        self.possible_actions = possible_actions
+
+    def set_actions(self):
+        num_tiles = len(self.gameboard.tiles)
+        self.action_matrix = np.zeros((num_tiles, self.gameboard.N_col * 4))
+
+        for tile_id, tile in enumerate(self.gameboard.tiles):
+            num_rotations = len(tile)
+            for rotation in range(num_rotations):
+                # Find possible places to drop given rotation 
+                start_index = rotation * self.gameboard.N_col
+                end_index =  start_index + self.gameboard.N_col - len(tile[rotation]) + 1 
+                self.action_matrix[tile_id][start_index : end_index] = 1
+
 
     def fn_load_strategy(self,strategy_file):
         pass
@@ -36,26 +43,29 @@ class TQAgent:
 
 
     def fn_read_state(self):
-        self.state = np.reshape(self.gameboard.board, (1, self.gameboard.N_row*self.gameboard.N_col))
-        self.state = tuple(np.append(self.state, self.gameboard.cur_tile_type))
+        self.state = tuple( np.append(self.gameboard.board.flatten(), self.gameboard.cur_tile_type) )
         if self.state not in self.qtable:
-            self.qtable[self.state] = np.zeros(int(sum(self.possible_actions[self.gameboard.cur_tile_type])))
+            num_legal_actions = int(sum(self.action_matrix[self.gameboard.cur_tile_type]))
+            self.qtable[self.state] = np.zeros(num_legal_actions)
+
 
     def fn_select_action(self):
-        index_possible_actions = np.argwhere(self.possible_actions[self.gameboard.cur_tile_type] == 1).flatten()
+        legal_action_indices = np.argwhere(self.action_matrix[self.gameboard.cur_tile_type] == 1).flatten()
         if np.random.rand() < self.epsilon:
-            self.action_index = random.choice(index_possible_actions)
+            self.action = random.choice(legal_action_indices) # TODO this should return a qtable index
         else:
-            self.action_index = np.argmax(self.qtable[self.state])
-        
-        action_index = index_possible_actions[self.action_index]
+            self.action =  np.argmax(self.qtable[self.state])
+        action_index = legal_action_indices[self.action]
         position_drop = action_index % self.gameboard.N_col
-        number_of_rotation = action_index // self.gameboard.N_col
-        self.gameboard.fn_move(position_drop, number_of_rotation)
-        
+        number_of_rotations = action_index // self.gameboard.N_col
 
+        self.action_index = action_index
+        self.gameboard.fn_move(position_drop, number_of_rotations)
+        
+        # TODO Broke my action 
     def fn_reinforce(self,old_state,reward):
-        self.qtable[old_state][self.action_index] += self.alpha*(reward + np.max(self.qtable[self.state]) - self.qtable[old_state][self.action_index])
+        self.qtable[old_state][self.action] += self.alpha*(reward + np.max(self.qtable[self.state]) - self.qtable[old_state][self.action])
+
 
     def fn_turn(self):
         if self.gameboard.gameover:
@@ -70,8 +80,8 @@ class TQAgent:
                     # Here you can save the rewards and the Q-table to data files for plotting of the rewards and the Q-table can be used to test how the agent plays
             if self.episode>=self.episode_count:
                 plt.plot(self.reward_tots)
-                plt.title("Agent playing", fontsize=20)
-                plt.xlabel("Epochs")
+                # plt.title("Tetris ", fontsize=20)
+                plt.xlabel("Episode")
                 plt.ylabel("Reward")
                 plt.show()
                 raise SystemExit(0)
