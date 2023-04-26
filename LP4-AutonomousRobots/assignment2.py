94634d12100dce42e4706c4a1859da68cfaacb84
ackejohansson <axelgbg@live.se>
2023-04-26 09:34:08 +0200

    Correctly drives and plots

--- LP4-AutonomousRobots/assignment2.py
+++ LP4-AutonomousRobots/assignment2.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 v0 = .5
 t1 = 10
 t2 = 5
-t = np.linspace(0, 10, 101)
+t = np.linspace(0, 10, 1001)
 vl = v0*(t/t1)
 vr = v0*(t/t2)
 R = .12
@@ -17,8 +17,11 @@ v = (vr+vl)/2
 
 for i in range(len(t)-1):
     theta[i+1] = theta[i] + theta_dot[i] * dt
-    x[i+1] = x[i] + v[i] * np.cos(theta[i+1]) * dt
-    y[i+1] = y[i] + v[i] * np.sin(theta[i+1]) * dt
+    x[i+1] = x[i] + v[i+1] * np.cos(theta[i+1]) * dt
+    y[i+1] = y[i] + v[i+1] * np.sin(theta[i+1]) * dt
 
 plt.plot(x,y)
+plt.plot(x[0], y[0], 'go', label='Start position') 
+plt.plot(x[-1], y[-1], 'ro', label='End position')
+plt.legend(loc='upper right')
 plt.show()
