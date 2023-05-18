5bec324d7a9227789a163b0798e33e7390c0312a
ackejohansson <axelgbg@live.se>
2023-05-18 17:28:54 +0200

    Now works some what

--- LP4-AutonomousRobots/Assignment 3/assignment3.py
+++ LP4-AutonomousRobots/Assignment 3/assignment3.py
@@ -45,55 +45,54 @@ def plot_motion(x_list, y_list, labels):
         filename = 'odometry_gnss_gt.png'
     if len(x_list) == 4:
        filename = 'kalman.png'
-    plt.savefig(os.path.join('figures', filename))
+    #plt.savefig(os.path.join('figures', filename))
     plt.show()
 
+def get_xhat(xhat, index, theta_dot, v):
+    xhat[0] = v[index]
+    u = np.array([0, theta_dot[index], xhat[0]*np.cos(xhat[1]), xhat[0]*np.sin(xhat[1])])*dt
+    return xhat + u
 
-def kalman_filter(vl, vr, gnns_dict, time, theta, v, x, y, theta_dot):
-    # initialize matricies
-    state = np.zeros((len(time), 2))
-    u = np.array([np.zeros([len(theta_dot)]), theta_dot, x, y])*dt
+def get_F(xhat):
+    return np.array([[1, 0, 0, 0],
+                     [0, 1, 0, 0],
+                     [(np.cos(xhat[1]))*dt, (-xhat[0]*np.sin(xhat[1]))*dt, 1, 0],
+                     [(np.sin(xhat[1]))*dt, (xhat[0]*np.cos(xhat[1]))*dt, 0, 1]])
 
-    F = np.array([[1, 0, 0, 0],
-                  [0, 1, 0, 0],
-                  [dt*np.cos(theta), v*np.sin(theta)*dt, 1, 0],
-                  [dt*np.sin(theta), -v*np.cos(theta)*dt, 0, 1]])
-    H = np.eye(4)
-    H[:2, :2] = 0
-    Q = np.eye(4)*0.01   
-    R = np.eye(4)*0.01
+def kalman_filter(gnns_dict, time, theta, v, x, y, theta_dot):
+    state = np.zeros((len(time), 4))
     P = np.eye(4)
+    H = P.copy()
+    H[:2, :2] = 0
+    Q = P * 0.01   
+    R = P * 0.1
 
-    # initialize state
     xhat = np.array([v[0], theta[0], x[0], y[0]])
     for index, t in enumerate(time):
-        xhat = xhat + u[:,index]
+        xhat = get_xhat(xhat, index, theta_dot, v)
+        F = get_F(xhat)    
         P = F @ P @ F.T + Q
         if t in gnns_dict:
             G = P @ H.T @ np.linalg.pinv(H @ P @ H.T + R)
-            xhat = xhat + G @ (gnns_dict[t] - xhat)
+            xhat += + G @ (np.array([0, 0, gnns_dict[t][0], gnns_dict[t][1]]) - xhat)
             P = (np.eye(4) - G @ H) @ P
-        state[index] = xhat[:2]
-    return state[0], state[1]
-            
+        state[index] = xhat
+    return state[:,2], state[:,3]
+
     
 
 def main():
     time, vl, vr = read_data('wheelspeeds-1.csv', 'time', 'vl', 'vr')
     t, gnss_x, gnss_y = read_data('gnss-1.csv', 't', 'x', 'y')
     gnss_dict = {t[i]: (gnss_x[i], gnss_y[i]) for i in range(len(t))}
-    # save dict with t as key and gnss_x, gnss_y as values
-    
     gt_x, gt_y, _ = read_data('groundtruth-1.csv', 'x', 'y', 'phi')
     
     theta_dot = (vr-vl)/(2*R)
     velocity = (vr+vl)/2
     x, y, theta = get_position(theta_dot, velocity)
 
-    kf_x, kf_y = kalman_filter(vl, vr, gnss_dict, time, theta, velocity, x, y, theta_dot)
-
+    kf_x, kf_y = kalman_filter(gnss_dict, time, theta, velocity, x, y, theta_dot)
     plot_motion([x, gnss_x, gt_x, kf_x], [y, gnss_y, gt_y, kf_y], ['odometry', 'gnss', 'ground truth', 'kalman'])
-
 
 
 if __name__== '__main__':
